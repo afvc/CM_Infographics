104edc87aeb53361129b779da5ad52f168d8644a
seniferraz <inesferra95@gmail.com>
2016-11-16 12:48:52 +0000

    HR & Ed Links + Soft Jumps

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-$("#Education").on("click", showHiddenEdu);
+/*-----EDUCATION-----*/
 
-function showHiddenEdu() {
-    if ($("#HiddenEdu").css("display") == "none") {
-        $("#HiddenEdu").css("display", "block");
+$("#Education").on("click", showHiddenEd);
+$(".Ed").on("click", showEd);
+
+function showHiddenEd() {
+    if ($("#HiddenEd").css("display") == "none") {
+        $("#HiddenEd").css("display", "block");
     } else {
-        $("#HiddenEdu").css("display", "none");
+        $("#HiddenEd").css("display", "none");
 
     }
-
 }
 
+function showEd() {
+    if ($("#HiddenEd").css("display") == "none") {
+        $("#HiddenEd").css("display", "block");
+    }
+}
+
+/*-----HUMAN RIGHTS-----*/
+
 $("#Rights").on("click", showHiddenHR);
+$(".HR").on("click", showHRs);
 
 function showHiddenHR() {
     if ($("#HiddenHR").css("display") == "none") {
@@ -19,5 +30,26 @@ function showHiddenHR() {
         $("#HiddenHR").css("display", "none");
 
     }
-
 }
+
+function showHRs() {
+    if ($("#HiddenHR").css("display") == "none") {
+        $("#HiddenHR").css("display", "block");
+    }
+}
+
+/*-----SOFT JUMPS-----*/
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
